09f88fc29da046e36467a4b76df1fe8a136b1493
Hoo Jun Zhe <hoojunzhe@gmail.com>
2026-07-29 20:02:38 +0800

    PDF and word

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"></path>
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 13l2 2 4-4"></path>
               </svg>
-              Download in PDF
+              PDF
             </a>
 
             {/* Word Download Button */}
@@ -96,7 +96,7 @@ function App() {
               <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
               </svg>
-              Download in Word
+              Word
             </a>
           </div>
         </section>
